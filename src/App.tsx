141d2e09032d84a37d816216b7d012e9412840ce
Elio Cortés <elio.cortes3000@gmail.com>
2026-01-27 21:13:53 -0500

    feat: initialize React project with Vite, TypeScript, and ESLint
    - Added package.json with dependencies for React, ReactDOM, and development tools.
    - Included Vite configuration for building and serving the application.
    - Created initial application structure with App component and basic styling.
    - Added SVG assets for React and Vite logos.
    - Configured TypeScript with strict settings and module resolution.
    - Set up ESLint for code linting and formatting.

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { memo, startTransition, useEffect, useState } from "react"
+const Child = memo(
+  function Child({ value, onClick }: { value: number; onClick: () => void })
+  {
+    console.log("Child Rendered")
+    useEffect(() =>
+    {
+      console.log("Child commited to DOM")
+    });
+    return <div>Child value: {value}</div>
+  }
+);
+const handleClick = () =>
+{
+  () => console.log("clicked")
+}
+
+export default function App()
+{
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState('light');
+  const slowLoop = performance.now();
+  while (performance.now() - slowLoop < 50) { }
+
+  console.log("App rendered");
+  useEffect(() =>
+  {
+    console.log("App commited to DOM")
+  });
+  return (
+    <>
+      <h1>Count:{count} </h1>
+      <button onClick={() =>
+      {
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+        console.log("clicked");
+      }} > Increment</button>
+      <Child value={count} onClick={handleClick} />
+      <button onClick={() => theme === "ligth" ? setTheme("dark") : setTheme("ligth")}>Change theme</button>
+      <div>Current theme: {theme}</div>
+      <button onClick={() =>
+      {
+        startTransition(() =>
+        {
+          setCount(c => c + 1);
+        });
+      }}
+      >Low Priority Update</button>
+    </>
+  );
+}
+
