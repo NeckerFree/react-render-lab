01eeec641bde27363768bfe5510234f074c77851
Elio Cortés <elio.cortes3000@gmail.com>
2026-01-28 22:31:37 -0500

    refactor: simplify App component by removing unused code and optimizing state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,31 @@
-import { memo, startTransition, useEffect, useState } from "react"
-import Counter from "./Components/Counter";
-const Child = memo(
-  function Child({ value, onClick }: { value: number; onClick: () => void })
-  {
-    console.log("Child Rendered")
-    useEffect(() =>
-    {
-      console.log("Child commited to DOM")
-    });
-    return <div>Child value: {value}</div>
-  }
-);
-const handleClick = () =>
-{
-  () => console.log("clicked")
-}
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export default function App()
 {
   const [count, setCount] = useState(0);
-  const [theme, setTheme] = useState('light');
-  const slowLoop = performance.now();
-  while (performance.now() - slowLoop < 50) { }
+  const countRef = useRef(count);
+  console.log("render");
 
-  console.log("App rendered");
+  // useLayoutEffect(() =>
+  // {
+  //   console.log("commit (layout effect)"); // Log en Commit phase
+
+  // });
   useEffect(() =>
   {
-    console.log("App commited to DOM")
+    countRef.current = count;
   });
-  return (
-    <>
-      <h1>Count:{count} </h1>
-      <Counter />
-      <button onClick={() =>
-      {
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-        console.log("clicked");
-      }} > Increment</button>
-      <Child value={count} onClick={handleClick} />
-      <button onClick={() => theme === "ligth" ? setTheme("dark") : setTheme("ligth")}>Change theme</button>
-      <div>Current theme: {theme}</div>
-      <button onClick={() =>
-      {
-        startTransition(() =>
-        {
-          setCount(c => c + 1);
-        });
-      }}
-      >Low Priority Update</button>
-    </>
-  );
+  useEffect(() =>
+  {
+    const id = setInterval(() =>
+    {
+      console.log("count:", countRef.current);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+
+  return <button onClick={() => setCount(c => c + 1)}>+</button>;
+  <label>{count}</label>;
 }
 
